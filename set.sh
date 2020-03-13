c5fa823d9e5f72bac900fbced79f1b4e303cdf8b
David Hong <gdavid0510@gmail.com>
2020-03-13 21:01:22 +0900

    add pwm path for odroid u3 with ubuntu 18.04

--- set.sh
+++ set.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# For ODROID U3
 TEMP="/sys/devices/virtual/thermal/thermal_zone0/temp"
 MODE="/sys/devices/platform/odroidu2-fan/fan_mode"
 PWM="/sys/devices/platform/odroidu2-fan/pwm_duty"
+
+#For ODROID U3 18.04
+#PWM="/sys/devices/platform/pwm_fan/hwmon/hwmon0/pwm1"
 
 if [ -z $1 ] ; then
 	echo "Parameter: PWM speed (0~255)"
