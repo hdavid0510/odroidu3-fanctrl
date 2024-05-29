3c95d9ffd6e742ff25b5a5c5431392f90f6d9d32
David.Hong <hdavid0510@users.noreply.github.com>
2024-05-29 21:46:09 +0900

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-# For ODROID U3
 TEMP="/sys/devices/virtual/thermal/thermal_zone0/temp"
-MODE="/sys/devices/platform/odroidu2-fan/fan_mode"
-PWM="/sys/devices/platform/odroidu2-fan/pwm_duty"
 
-#For ODROID U3 18.04
-#PWM="/sys/devices/platform/pwm-fan/hwmon/hwmon0/pwm1"
+# For old ODROID U3 system images
+#MODE="/sys/devices/platform/odroidu2-fan/fan_mode"
+#PWM="/sys/devices/platform/odroidu2-fan/pwm_duty"
 
-if [ -z $1 ] ; then
+#For recent/community built ODROID U3 system images (Ubuntu 18.04~)
+MODE="/sys/devices/platform/pwm-fan/fan_mode"
+PWM="/sys/devices/platform/pwm-fan/hwmon/hwmon0/pwm1"
+
+if [ "$EUID" -ne 0 ] ; then
+	echo "Please run as root"
+ 	exit 2
+elif [ -z $1 ] ; then
 	echo "Parameter: PWM speed (0~255)"
 	exit 1
 elif [ $1 -gt 255 ] || [ $1 -lt 0 ] ; then
 	echo "PWM speed invalid (0~255)"
 	exit 1
 fi
+
+#echo "Updating PWM control mode: $MODE"
 echo manual > $MODE
+#echo "Updating PWM value: $PWM"
 echo $1 > $PWM
